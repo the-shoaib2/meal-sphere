65c37734c245fcb78cf0ab43b597776fa05ffdf5
MD Shoaib Khan <abrohoman019@gmail.com>
2025-06-22 01:28:52 +0600

    Update auth layout to redirect unauthenticated users to the login page instead of the home page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,7 +18,7 @@ export default async function AuthLayout({
   const session = await getServerSession(authOptions)
   
   if (!session) {
-    redirect('/')
+    redirect('/login')
   }
 
   return (
